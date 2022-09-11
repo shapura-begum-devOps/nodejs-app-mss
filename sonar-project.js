aac5800b501f306c1b69fa70f6a59426065f7ec4
Shapurabegum Shalavadi <shapuras12@gmail.com>
2022-09-11 11:51:40 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.229.47:9000/',
+  serverUrl: 'http://3.110.86.148:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
